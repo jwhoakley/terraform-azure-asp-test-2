ad1fcdb0b6d5621faca8ffa1581c8cb1d4d97912
jwhoakley <william@jwho.co.uk>
2024-02-03 12:25:44 +0000

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "webapp_ipv4_address" {
-  value = azurerm_linux_web_app.outbound_ip_address_list
-}
-
-output "webapp_hostname" {
-  value = azurerm_linux_web_app.default_hostname
-}
-
-output "webapp_domain" {
-  value = azurerm_linux_web_app.custom_domain_verification_id
+output "url" {
+  value = "${azurerm_app_service.example.name}.azurewebsites.net"
 }
