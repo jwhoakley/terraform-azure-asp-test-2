93a25222d05e1c5ea2a4484d1266722ea9919811
Will Oakley <william@jwho.co.uk>
2024-02-07 15:31:51 +0000

    Update outputs.tf
    Correcting the URL output to TF cloud

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,3 @@
 output "url" {
   value = "${azurerm_linux_web_app.webapp.name}.azurewebsites.net"
 }
-output "url-asp" {
-  value = "${azurerm_service_plan.appserviceplan.name}.azurewebsites.net"
-}
