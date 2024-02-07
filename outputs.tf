a0231155d695bf011bde3daf240057a9ea73d472
Will Oakley <william@jwho.co.uk>
2024-02-07 15:27:03 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "url" {
-  value = "${azurerm_linux_web_app.name}.azurewebsites.net"
+  value = "${azurerm_linux_web_app.webapp.name}.azurewebsites.net"
 }
 output "url-asp" {
   value = "${azurerm_service_plan.appserviceplan.name}.azurewebsites.net"
